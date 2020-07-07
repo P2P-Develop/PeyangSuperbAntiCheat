84f4a7f43ae9ecf4032335110d9cdaa04fd93d91
Potato1682 <contact@potato1682.ml>
2020-07-07 18:44:42 +0900

    Update Help.java

--- src/main/java/ml/peya/plugins/Commands/CmdPub/Help.java
+++ src/main/java/ml/peya/plugins/Commands/CmdPub/Help.java
@@ -9,53 +9,53 @@ public class Help
     public static void run(CommandSender sender, String label)
     {
         boolean flag = false;
-        sender.sendMessage(MessageEngihe.get("base.prefix"));
+        sender.sendMessage(MessageEngine.get("base.prefix"));
 
         if (sender.hasPermission("psac.report"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.report", MessageEngihe.hsh("label", label)));
+            sender.sendMessage(MessageEngine.get("command.help.report", MessageEngihe.hsh("label", label)));
             flag = true;
         }
 
         if (sender.hasPermission("psac.view"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.view", MessageEngihe.hsh("label", label)));
+            sender.sendMessage(MessageEngine.get("command.help.view", MessageEngihe.hsh("label", label)));
             flag = true;
         }
 
         if (sender.hasPermission("psac.aurapanic"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.aurapanic"));
+            sender.sendMessage(MessageEngine.get("command.help.aurapanic"));
             flag = true;
         }
 
         if (sender.hasPermission("psac.aurabot"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.aurabot"));
+            sender.sendMessage(MessageEngine.get("command.help.aurabot"));
             flag = true;
         }
 
         if (sender.hasPermission("psac.show"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.show", MessageEngihe.hsh("label", label)));
+            sender.sendMessage(MessageEngine.get("command.help.show", MessageEngihe.hsh("label", label)));
             flag = true;
         }
 
         if (sender.hasPermission("psac.drop"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.drop", MessageEngihe.hsh("label", label)));
+            sender.sendMessage(MessageEngine.get("command.help.drop", MessageEngihe.hsh("label", label)));
             flag = true;
         }
 
         if (sender.hasPermission("psac.kick"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.kick", MessageEngihe.hsh("label", label)));
+            sender.sendMessage(MessageEngine.get("command.help.kick", MessageEngihe.hsh("label", label)));
             flag = true;
         }
 
         if (sender.hasPermission("psac.bans"))
         {
-            sender.sendMessage(MessageEngihe.get("command.help.bans"));
+            sender.sendMessage(MessageEngine.get("command.help.bans"));
             flag = true;
         }
 
@@ -63,10 +63,10 @@ public class Help
             return;
 
         if (sender.hasPermission("psac.drop") || sender.hasPermission("psac.show"))
-            sender.sendMessage(MessageEngihe.get("command.help.mngIdWarning"));
+            sender.sendMessage(MessageEngine.get("command.help.mngIdWarning"));
 
 
         if (!flag)
-            sender.sendMessage(MessageEngihe.get("error.psac.notPage"));
+            sender.sendMessage(MessageEngine.get("error.psac.notPage"));
     }
 }
