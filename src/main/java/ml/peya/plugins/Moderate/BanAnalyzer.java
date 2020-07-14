2384f386343eb95486cc272f4112f2db6d410532
Potato1682 <contact@potato1682.ml>
2020-07-14 12:27:40 +0900

    Issue修正&コード短縮

--- src/main/java/ml/peya/plugins/Moderate/BanAnalyzer.java
+++ src/main/java/ml/peya/plugins/Moderate/BanAnalyzer.java
@@ -22,16 +22,7 @@ public class BanAnalyzer
                     ResultSet set = statement.executeQuery("SeLeCt * FrOm KiCk WhErE UUID='" + uuid.toString() + "'");
                     while(set.next())
                     {
-                        String name = set.getString("PLAYER");
-                        String id = set.getString("UUID");
-                        String kickId = set.getString("KICKID");
-
-
-                        long date  = set.getLong("DATE");
-                        String reason = set.getString("REASON");
-
-                        Bans bans = new Bans(date, reason, name, id, kickId.replace("#", ""), Type.KICK);
-                        abuses.add(bans);
+                        abuses.add(new Bans(set.getLong("DATE"), set.getString("REASON"), set.getString("PLAYER"), set.getString("UUID"), set.getString("KICKID").replace("#", ""), Type.KICK););
                     }
                 }
                 catch (Exception e)
@@ -43,9 +34,9 @@ public class BanAnalyzer
                     break;
             case BAN:
                 break;
+            default:
+                return abuses;
         }
-
-        return abuses;
     }
     public enum Type
     {
