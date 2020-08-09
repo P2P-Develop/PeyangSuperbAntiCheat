0b19636e3a950566ea7b5143f6a5f0559fa8158b
peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>
2020-08-09 09:16:44 +0900

    temp
    Signed-off-by: peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>

--- src/main/java/ml/peya/plugins/Moderate/TrustModifier.java
+++ src/main/java/ml/peya/plugins/Moderate/TrustModifier.java
@@ -13,42 +13,14 @@ import java.sql.*;
  */
 public class TrustModifier
 {
-    /** addって書いてるけどほんとはtoggleなやつ。なかったらaddするしあったらremoveする。
+    /**
+     * addって書いてるけどほんとはtoggleなやつ。なかったらaddするしあったらremoveする。
+     *
      * @param player 切り替えするプレイヤー。
      * @param sender イベントsender。
      */
     public static void addTrustPlayer(Player player, CommandSender sender)
     {
-        try (Connection connection = PeyangSuperbAntiCheat.trust.getConnection();
-             Statement statement = connection.createStatement())
-        {
-            ResultSet rs = statement.executeQuery("SeLeCt * FrOm TrUsT wHeRe PLAYER='" + player.getName() + "'");
-            if (rs.next())
-            {
-                statement.executeQuery("DeLeTe FrOm TrUsT wHeRe PLAYER='" + player.getName() + "'");
-                sender.sendMessage(MessageEngine.get("message.trust.remove", MessageEngine.hsh("player", player.getName())));
-            }
-            else
-            {
-                statement.executeQuery("InSeRt InTo TrUsT (PLAYER) vAlUe ('" + player.getName() + "');");
-                sender.sendMessage(MessageEngine.get("message.trust.add", MessageEngine.hsh("player", player.getName())));
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            ReportUtils.errorNotification(ReportUtils.getStackTrace(e));
-        }
-    }
-
-    /** プレイヤーが信用されてるか調べる。
-     * @param player 信用されてる気がするプレイヤー。
-     *
-     * @return 信用されてたらtrue。
-     */
-    public static boolean isTrusted(Player player)
-    {
-        final boolean[] result = { true };
 
         new BukkitRunnable()
         {
@@ -59,16 +31,51 @@ public class TrustModifier
                      Statement statement = connection.createStatement())
                 {
                     ResultSet rs = statement.executeQuery("SeLeCt * FrOm TrUsT wHeRe PLAYER='" + player.getName() + "'");
-                    result[0] = rs.next();
+                    if (rs.next())
+                    {
+                        statement.executeQuery("DeLeTe FrOm TrUsT wHeRe PLAYER='" + player.getName() + "'");
+                        sender.sendMessage(MessageEngine.get("message.trust.remove", MessageEngine.hsh("player", player.getName())));
+                    }
+                    else
+                    {
+                        statement.executeQuery("InSeRt InTo TrUsT vAlUe ('" + player.getName() + "');");
+                        sender.sendMessage(MessageEngine.get("message.trust.add", MessageEngine.hsh("player", player.getName())));
+                    }
                 }
                 catch (Exception e)
                 {
                     e.printStackTrace();
                     ReportUtils.errorNotification(ReportUtils.getStackTrace(e));
                 }
+                this.cancel();
             }
         }.runTask(PeyangSuperbAntiCheat.getPlugin());
 
-        return result[0];
+    }
+
+    /**
+     * プレイヤーが信用されてるか調べる。
+     *
+     * @param player 信用されてる気がするプレイヤー。
+     * @return 信用されてたらtrue。
+     */
+    public static boolean isTrusted(Player player)
+    {
+        boolean result = false;
+
+        try (Connection connection = PeyangSuperbAntiCheat.trust.getConnection();
+             Statement statement = connection.createStatement())
+        {
+            ResultSet rs = statement.executeQuery("SeLeCt * FrOm TrUsT wHeRe PLAYER='" + player.getName() + "'");
+            result = rs.next();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            ReportUtils.errorNotification(ReportUtils.getStackTrace(e));
+        }
+
+
+        return result;
     }
 }
