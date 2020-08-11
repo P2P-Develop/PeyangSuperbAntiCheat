ed65605c647c050c20e2aa336b068f5c83c02d51
Potato1682 <contact@potato1682.ml>
2020-08-11 12:56:41 +0900

    Update TargetStick.java

--- src/main/java/ml/peya/plugins/Gui/Items/Main/TargetStick.java
+++ src/main/java/ml/peya/plugins/Gui/Items/Main/TargetStick.java
@@ -10,8 +10,17 @@ import org.bukkit.inventory.meta.*;
 
 import static ml.peya.plugins.Utils.LookingUtils.getLookingEntity;
 
+/**
+ * ターゲットを再設定するユーティリティアイテム(ブレイズロッド)を管理します。
+ */
 public class TargetStick implements IItems
 {
+    /**
+     * イベント発動時の処理をオーバーライドします。
+     *
+     * @param player メンチを切る側のプレイヤー。
+     * @param target オーバーライドのために必要だと思われる。実際は必要ない。
+     */
     @Override
     public void run(Player player, String target)
     {
@@ -24,6 +33,13 @@ public class TargetStick implements IItems
         player.performCommand("target " + lookingPlayer.getName());
     }
 
+    /**
+     * アイテムを取得する関数のオーバーライド。どのようなアイテムを返すか、どのような動きをするか、などと言った詳細をこの関数で設定し、アイテムとして返す。
+     *
+     * @param target ターゲットが誰であるか。
+     *
+     * @returns 関数内の処理によって設定されたアイテム。
+     */
     @Override
     public ItemStack getItem(String target)
     {
@@ -39,18 +55,33 @@ public class TargetStick implements IItems
         return stack;
     }
 
+    /**
+     * インベントリに空きスペースがあるかどうかを確認する関数のオーバーライド。この関数は使わないため実装は不要。
+     *
+     * @returns 実装は不要なためfalse。
+     */
     @Override
     public boolean canSpace()
     {
         return false;
     }
 
+    /**
+     * どのようなIDであるか取得する。詳細はPSACドキュメントを参照。
+     *
+     * @returns このアイテムの実行ID。
+     */
     @Override
     public String getExecName()
     {
         return "TARGET_STICK";
     }
 
+    /**
+     * どのようなタイプであるか取得する。
+     *
+     * @returns ほぼMAIN。大体MAIN。
+     */
     @Override
     public Type getType()
     {
