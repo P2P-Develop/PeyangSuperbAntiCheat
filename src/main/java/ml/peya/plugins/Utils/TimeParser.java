ac16e9b680b333ed5973911233d09854bb556469
peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>
2020-10-10 20:18:48 +0900

    りばーっっと

--- src/main/java/ml/peya/plugins/Utils/TimeParser.java
+++ src/main/java/ml/peya/plugins/Utils/TimeParser.java
@@ -22,20 +22,20 @@ public class TimeParser
     public static Date convert(String... args)
     {
         Calendar c = Calendar.getInstance();
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         for (String arg : args)
         {
-            Matcher m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(arg);
-
+            Matcher m = p.matcher(arg);
             if (!m.find())
                 continue;
 
             String unit = m.group(1);
-
             int num;
             try
             {
                 num = Integer.parseInt(arg.replace(unit, "").replace("s", ""));
+
             }
             catch (Exception ignored)
             {
@@ -44,33 +44,27 @@ public class TimeParser
 
             switch (unit.toLowerCase())
             {
-                case "years":
                 case "year":
                 case "y":
                     c.add(Calendar.YEAR, num);
                     break;
-                case "months":
                 case "month":
                 case "mo":
                     c.add(Calendar.MONTH, num);
                     break;
-                case "days":
                 case "day":
                 case "d":
                     c.add(Calendar.DAY_OF_MONTH, num);
                     break;
-                case "hours":
                 case "hour":
                 case "h":
                     c.add(Calendar.HOUR, num);
                     break;
-                case "minutes":
                 case "minute":
                 case "min":
                 case "m":
                     c.add(Calendar.MINUTE, num);
                     break;
-                case "seconds":
                 case "second":
                 case "sec":
                 case "s":
@@ -90,15 +84,20 @@ public class TimeParser
      */
     public static String convertFromDate(Date date)
     {
-        final long diff = date.getTime() - new Date().getTime();
+        String d = MessageEngine.get("base.day");
+        String h = MessageEngine.get("base.hour");
+        String m = MessageEngine.get("base.minutes");
+        String s = MessageEngine.get("base.seconds");
 
-        return diff / (24 * 60 * 60 * 1000) +
-                MessageEngine.get("base.day") +
-                diff / (60 * 60 * 1000) % 24 +
-                MessageEngine.get("base.hour") +
-                diff / (60 * 1000) % 60 +
-                MessageEngine.get("base.minutes") +
-                diff / 1000 % 60 +
-                MessageEngine.get("base.seconds");
+        long now = new Date().getTime();
+        long ago = date.getTime();
+        long diff = ago - now;
+
+        long day = diff / (24 * 60 * 60 * 1000);
+        long hour = diff / (60 * 60 * 1000) % 24;
+        long minute = diff / (60 * 1000) % 60;
+        long second = diff / 1000 % 60;
+
+        return day + d + hour + h + minute + m + second + s;
     }
 }
