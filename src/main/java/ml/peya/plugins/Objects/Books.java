baa8771b3d7d6e81a5bafd0abde8ab7553f21236
peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-09 10:24:09 +0900

    Re: format

--- src/main/java/ml/peya/plugins/Objects/Books.java
+++ src/main/java/ml/peya/plugins/Objects/Books.java
@@ -1,29 +1,19 @@
 package ml.peya.plugins.Objects;
 
-import ml.peya.plugins.Enum.EnumCheatType;
-import ml.peya.plugins.Utils.SeverityLevels;
-import ml.peya.plugins.Utils.TextBuilder;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
+import ml.peya.plugins.Enum.*;
+import ml.peya.plugins.Utils.*;
+import net.md_5.bungee.api.chat.*;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.*;
 
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.stream.Collectors;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.stream.*;
 
-import static ml.peya.plugins.Utils.MessageEngine.get;
-import static ml.peya.plugins.Utils.MessageEngine.pair;
+import static ml.peya.plugins.Utils.MessageEngine.*;
 
 /**
  * 本のアイテムをまとめたやつ
@@ -71,6 +61,7 @@ public class Books
                 ClickEvent.Action.RUN_COMMAND,
                 "/report " + player.getName() + " " + tmpReasonText + " -B"
             ))
+            .append("\n")
             .append(get("reportbook.cancel"))
             .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/report $$cancel$$"));
 
